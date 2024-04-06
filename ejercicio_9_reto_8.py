5bdd8d4dd5e84e724f836a25a0dbc9b63a320140
JavierGONL <159032556+JavierGONL@users.noreply.github.com>
2024-04-06 16:21:09 -0500

    Add files via upload

--- ejercicio_9_reto_8.py
+++ ejercicio_9_reto_8.py
@@ -10,7 +10,12 @@ def funcion_seno(numeroX: float, numeroN: float = 20) -> float:
         seno_serie_maclaurin = ((-1) ** n) * ((numeroX) ** (2 * n + 1)) / factorial(2 * n + 1)
         resultado += seno_serie_maclaurin
     return resultado
-
+def rango_de_error(numeroX, numeroN):
+    maclaurin = funcion_seno(numeroX, numeroN)
+    math_sin = math.sin(numeroX)
+    error_relativo = (math_sin - maclaurin) / math_sin
+    porcentaje_error = abs(error_relativo) * 100
+    return porcentaje_error    
 if __name__ == "__main__":
     numeroX = float(input("ingrese un numero: "))
-    print(f"el resultado aproximado de la funcion seno es: {funcion_seno(numeroX)}, el verdadero es: {math.sin(numeroX)}")
+    print(f"el resultado aproximado de la funcion seno es: {funcion_seno(numeroX)}, el verdadero es: {math.sin(numeroX)} y el rango de error es {rango_de_error(numeroX, 20)}%")    
